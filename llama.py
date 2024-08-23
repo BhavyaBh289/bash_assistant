ee9b781218e767c4561ecce898d76db6c526ada7
BhavyaBh289 <shahbhavyasp@gmail.com>
2024-08-23 12:32:46 +0530

    lora added

--- llama.py
+++ llama.py
@@ -2,6 +2,7 @@ from llama_cpp import Llama
 import json
 import torch
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, AdamW,AutoModelForCausalLM
+from peft import get_peft_model, LoraConfig, TaskType
 
 # model_path = r'C:\Users\Sakshi Shewale\OneDrive\Desktop\gguf\llama-2-7b.Q4_K_M.gguf'
 model_name="/home/bh289/Documents/clg/sem 7/bash_assistant/starcoderbase-local"
@@ -12,7 +13,7 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 # printing model info
 # hidden_size = model.config.hidden_size
 # print(hidden_size)
-# layer_names = model.state_dict().keys()
+layer_names = model.state_dict().keys()
 
 # for name in layer_names:
     # print(name)
@@ -35,12 +36,25 @@ output_texts = [d['bash_code'] for d in data]
 inputs = tokenizer(input_texts, return_tensors="pt", padding=True, truncation=True, max_length=512)
 outputs = tokenizer(output_texts, return_tensors="pt", padding=True, truncation=True, max_length=512)
 
+lora_config = LoraConfig(
+    task_type=TaskType.CAUSAL_LM,  # Since you're working with a language model
+    r=16,
+    lora_alpha=32,
+    lora_dropout=0.1,
+    target_modules=[
+        "attn.c_attn",
+        "attn.c_proj",
+        "mlp.c_fc",
+        "mlp.c_proj"
+    ]
+)
 
+model = get_peft_model(model, lora_config)
 
 optimizer = AdamW(model.parameters(), lr=5e-5)
 
 model.train()
-for epoch in range(3):  # Define the number of epochs
+for epoch in range(3):
     optimizer.zero_grad()
     outputs = model(input_ids=inputs['input_ids'], labels=outputs['input_ids'])
     loss = outputs.loss
@@ -49,4 +63,4 @@ for epoch in range(3):  # Define the number of epochs
 
     print(f"Epoch {epoch+1}, Loss: {loss.item()}")
 
-model.save_pretrained('fine-tuned-gguf-model')
+model.save_pretrained('fine-tuned-lora-model')
